ddf6e64ec7043b258edec68df9c068e5b05a3b3c
Alexandr Morozov <morozov1982@gmail.com>
2021-09-16 10:29:08 +0600

    add: lesson_01

---
+++ backend/library/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=46)
+    last_name = models.CharField(max_length=46)
+    birthday_year = models.PositiveIntegerField()
