b575b91ff5d1b85a6c98e646c9230792d16c84b3
Morozov Alexandr <morozov1982@gmail.com>
2021-10-28 11:20:15 +0600

    add: lesson_12 classwork files

--- backend/backend/settings_local.py
+++ backend/backend/settings_local.py
@@ -127,14 +127,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'library',
-        'USER': 'admin',
-        'PASSWORD': '123',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '24322',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
